daea4e2fac348a325f5e448a446329cdbd3421d8
katampoi <simonkatampoi@student.moringaschool.com>
2022-09-26 13:42:50 +0300

    Complete Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name: 'simon', last_name: 'katampoi', grade: 70)
+Student.create!(first_name: 'martin', last_name: 'mwangi', grade: 70)
+Student.create!(first_name: 'grace', last_name: 'wangui', grade: 70)
+Student.create!(first_name: 'joseph', last_name: 'arikon', grade: 70)
+Student.create!(first_name: 'samuel', last_name: 'maingi', grade: 70)
+Student.create!(first_name: 'Dwayne', last_name: 'Johnson', grade: 70)
